9bf340b902fe256422da3a33879c3709c53813d0
anthony1234p <119891367+anthony1234p@users.noreply.github.com>
2023-02-16 13:33:55 -0500

    Good

---
+++ PS1/src/CHEME-1800-ProblemSet-1-Lib.jl
@@ -0,0 +1,18 @@
+"""
+    encrypt(plaintext::String) -> Dict{Int64,String}
+
+Fill me in
+"""
+function encrypt(plaintext::String)::Dict{Int64,String}
+end
+
+#CODE CODE CODE
+
+
+"""
+    decrypt(encrypteddata::Dict{Int64,String}) -> String
+
+Fill me in
+"""
+function decrypt(encrypteddata::Dict{Int64,String})::String
+end
